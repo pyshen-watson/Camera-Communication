27d0d95fe8fee5e21298695a12e6ec485062435f
pyshen <pyshen.watson@gmail.com>
2021-12-18 12:59:11 +0800

    Make main more like step-by-step

--- main.py
+++ main.py
@@ -3,16 +3,20 @@ import csv
 from modules.video2signal import video2signal
 
 
-if __name__ == '__main__':
+def convert_video_to_signal_csv(video_name, save=True):
 
-    cap = cv2.VideoCapture('video.mp4')
+    cap = cv2.VideoCapture(video_name)
     print('Convert video to signal')
     signals = video2signal(cap)
     cap.release()
     print('Convert done.')
 
+    if save:
+        with open('./signal.csv', 'w') as file:
+            writer = csv.writer(file)
+            writer.writerows(signals)
 
-    with open('./signal.csv', 'w') as file:
-        writer = csv.writer(file)
-        writer.writerows(signals)
+if __name__ == '__main__':
+
+   convert_video_to_signal_csv('video.mp4', save=False)
     
